db41a01d970df07f4d1023555f2aa30570553f27
Applywizz-git <applywizztechportfolios@gmail.com>
2025-09-23 10:45:59 +0530

    Update settings.py

--- profile_scoring/settings.py
+++ profile_scoring/settings.py
@@ -15,9 +15,11 @@ environ.Env.read_env(BASE_DIR / ".env")
 # Security settings
 # =====================
 SECRET_KEY = env('DJANGO_SECRET_KEY')
-DEBUG = env.bool('DEBUG', default=False)  # Use the DEBUG value from the .env file
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['profile-scoring-wxo6.onrender.com'])
+DEBUG = True
+
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', 'profile-scoring-wxo6.onrender.com'])
 CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=['https://profile-scoring-wxo6.onrender.com'])
+
 
 # =====================
 # Application definition
